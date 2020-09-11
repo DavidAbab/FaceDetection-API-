e7dd32a8a227d7dce9d9341044251c01ea339576
DavidAbab <david.abab@etu.hesge.ch>
2020-09-12 00:44:37 +0200

    probleme avec n our server, the handleApiCall  returns a string in the catch block which will evaluate to true.

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const handleApiCall = (req, res) => {
     .then((data) => {
       res.json(data);
     })
-    .catch((err) => res.status(400).json("Problème avec l api"));
+    .catch((err) => res.status(400).json(false));
 };
 
 app.get("/", (req, res) => {
